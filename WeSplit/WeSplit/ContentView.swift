cda073c40c31dca913cdd3809fd0ad96462b8feb
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2019-10-15 21:59:02 -0400

    Start Day 16

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Tianna Henry-Lewis on 2019-10-15.
+//  Copyright © 2019 Tianna Henry-Lewis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
