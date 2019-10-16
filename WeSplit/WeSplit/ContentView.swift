836596f4adfc09941c3d620b96aad0de7636b60e
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2019-10-15 22:26:58 -0400

    Start - Day 17

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,68 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    //@State private var tapCount = 0
-    
-    //@State private var name = ""
-    
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
-    
     var body: some View {
-        
-        Picker("Select Your Student", selection: $selectedStudent) {
-            ForEach (0 ..< students.count) {
-                Text(self.students[$0])
-            }
-        }
-        
-//        Form {
-//            ForEach(0 ..< 100) {
-//                Text("Row \($0)")
-//            }
-//        }
-        
-//        Form {
-//            TextField("Enter Your Name", text: $name)
-//            Text("Hello \(name)")
-//        }
-        
-        /*
-        If observing elements below, comment out the below
-        botton element code
-        */
-//        Button("Tap Count: \(tapCount)") {
-//            self.tapCount += 1
-//        }
-        
-        /*
-         Uncomment to observe NavigationView, Form, Section,
-         Group, and Text Elements
-        */
-        
-//                NavigationView {
-//                    Form {
-//                        Section{
-//                            Group {
-//                                Text("Hello World")
-//                                Text("Hello World")
-//                            }
-//                            Group {
-//                                Text("Hello World")
-//                                Text("Hello World")
-//                            }
-//                        }
-//                        Section {
-//                            Text("Hello World")
-//                            Text("Hello World")
-//                            Text("Hello World")
-//                            Text("Hello World")
-//                            Text("Hello World")
-//                        }
-//                    }
-//                    .navigationBarTitle("SwiftUI")
-//                }
-        
+        Text("Hello World")
     }
 }
 
